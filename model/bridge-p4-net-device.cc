f4607589c2ed22ad6bad07b42cca805454877ad4
mingyu_ma <mingyu.ma@tu-dresden.de>
2024-11-28 07:48:59 +0100

    add the interface between the bridge and p4 process core

---
+++ model/bridge-p4-net-device.cc
@@ -0,0 +1,427 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2018 Stanford University
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors: Mingyu Ma <mingyu.ma@tu-dresden.de>
+ */
+
+#include "bridge-p4-net-device.h"
+
+#include "global.h"
+
+#include "ns3/boolean.h"
+#include "ns3/channel.h"
+#include "ns3/ethernet-header.h"
+#include "ns3/log.h"
+#include "ns3/node.h"
+#include "ns3/packet.h"
+#include "ns3/simulator.h"
+#include "ns3/uinteger.h"
+
+/**
+ * \file
+ * \ingroup bridge
+ * ns3::BridgeP4NetDevice implementation.
+ */
+
+namespace ns3
+{
+
+NS_LOG_COMPONENT_DEFINE("BridgeP4NetDevice");
+
+NS_OBJECT_ENSURE_REGISTERED(BridgeP4NetDevice);
+
+TypeId
+BridgeP4NetDevice::GetTypeId()
+{
+    static TypeId tid = TypeId("ns3::BridgeP4NetDevice")
+                            .SetParent<NetDevice>()
+                            .SetGroupName("Bridge")
+                            .AddConstructor<BridgeP4NetDevice>()
+                            .AddAttribute("Mtu",
+                                          "The MAC-level Maximum Transmission Unit",
+                                          UintegerValue(1500),
+                                          MakeUintegerAccessor(&BridgeP4NetDevice::SetMtu,
+                                                               &BridgeP4NetDevice::GetMtu),
+                                          MakeUintegerChecker<uint16_t>());
+    return tid;
+}
+
+BridgeP4NetDevice::BridgeP4NetDevice()
+    : m_node(nullptr),
+      m_ifIndex(0)
+{
+    NS_LOG_FUNCTION_NOARGS();
+    m_channel = CreateObject<P4BridgeChannel>();
+
+    m_p4Switch = new P4Switch(this);
+
+    // \TODO interface init & LOCAL_CALL
+
+    std::string jsonPath = P4GlobalVar::g_p4JsonPath;
+    std::vector<char*> args;
+    args.push_back(nullptr);
+    args.push_back(jsonPath.data());
+
+    m_p4Switch->init(static_cast<int>(args.size()), args.data());
+    m_p4Switch->start_and_return_();
+
+    NS_LOG_LOGIC("A P4 Netdevice was initialized.");
+}
+
+BridgeP4NetDevice::~BridgeP4NetDevice()
+{
+    NS_LOG_FUNCTION_NOARGS();
+}
+
+void
+BridgeP4NetDevice::DoDispose()
+{
+    NS_LOG_FUNCTION_NOARGS();
+    for (auto iter = m_ports.begin(); iter != m_ports.end(); iter++)
+    {
+        *iter = nullptr;
+    }
+    m_ports.clear();
+    m_channel = nullptr;
+    m_node = nullptr;
+    NetDevice::DoDispose();
+}
+
+void
+BridgeP4NetDevice::ReceiveFromDevice(Ptr<NetDevice> incomingPort,
+                                     Ptr<const Packet> packet,
+                                     uint16_t protocol,
+                                     const Address& src,
+                                     const Address& dst,
+                                     PacketType packetType)
+{
+    NS_LOG_FUNCTION_NOARGS();
+    NS_LOG_DEBUG("UID is " << packet->GetUid());
+
+    Mac48Address src48 = Mac48Address::ConvertFrom(src);
+    Mac48Address dst48 = Mac48Address::ConvertFrom(dst);
+
+    if (!m_promiscRxCallback.IsNull())
+    {
+        m_promiscRxCallback(this, packet, protocol, src, dst, packetType);
+    }
+
+    if (dst48 == m_address)
+    {
+        m_rxCallback(this, packet, protocol, src);
+    }
+
+    int inPort = GetPortNumber(incomingPort);
+
+    EthernetHeader eeh;
+    eeh.SetDestination(dst48);
+    eeh.SetSource(src48);
+    eeh.SetLengthType(protocol);
+
+    Ptr<ns3::Packet> ns3Packet((ns3::Packet*)PeekPointer(packet));
+    ns3Packet->AddHeader(eeh);
+
+    m_p4Switch->ReceivePacket(ns3Packet, inPort, protocol, dst);
+}
+
+uint32_t
+BridgeP4NetDevice::GetNBridgePorts() const
+{
+    NS_LOG_FUNCTION_NOARGS();
+    return m_ports.size();
+}
+
+Ptr<NetDevice>
+BridgeP4NetDevice::GetBridgePort(uint32_t n) const
+{
+    NS_LOG_FUNCTION_NOARGS();
+    return m_ports[n];
+}
+
+void
+BridgeP4NetDevice::AddBridgePort(Ptr<NetDevice> bridgePort)
+{
+    NS_LOG_FUNCTION_NOARGS();
+    NS_ASSERT(bridgePort != this);
+    if (!Mac48Address::IsMatchingType(bridgePort->GetAddress()))
+    {
+        NS_FATAL_ERROR("Device does not support eui 48 addresses: cannot be added to bridge.");
+    }
+    if (!bridgePort->SupportsSendFrom())
+    {
+        NS_FATAL_ERROR("Device does not support SendFrom: cannot be added to bridge.");
+    }
+    if (m_address == Mac48Address())
+    {
+        m_address = Mac48Address::ConvertFrom(bridgePort->GetAddress());
+    }
+
+    NS_LOG_DEBUG("RegisterProtocolHandler for " << bridgePort->GetInstanceTypeId().GetName());
+
+    m_node->RegisterProtocolHandler(MakeCallback(&BridgeP4NetDevice::ReceiveFromDevice, this),
+                                    0,
+                                    bridgePort,
+                                    true);
+    m_ports.push_back(bridgePort);
+    m_channel->AddChannel(bridgePort->GetChannel());
+}
+
+uint32_t
+BridgeP4NetDevice::GetPortNumber(Ptr<NetDevice> port) const
+{
+    int portsNum = GetNBridgePorts();
+    for (int i = 0; i < portsNum; i++)
+    {
+        if (GetBridgePort(i) == port)
+            NS_LOG_DEBUG("Port found: " << i);
+        return i;
+    }
+    NS_LOG_ERROR("Port not found");
+    return -1;
+}
+
+void
+BridgeP4NetDevice::SetIfIndex(const uint32_t index)
+{
+    NS_LOG_FUNCTION_NOARGS();
+    m_ifIndex = index;
+}
+
+uint32_t
+BridgeP4NetDevice::GetIfIndex() const
+{
+    NS_LOG_FUNCTION_NOARGS();
+    return m_ifIndex;
+}
+
+Ptr<Channel>
+BridgeP4NetDevice::GetChannel() const
+{
+    NS_LOG_FUNCTION_NOARGS();
+    return m_channel;
+}
+
+void
+BridgeP4NetDevice::SetAddress(Address address)
+{
+    NS_LOG_FUNCTION_NOARGS();
+    m_address = Mac48Address::ConvertFrom(address);
+}
+
+Address
+BridgeP4NetDevice::GetAddress() const
+{
+    NS_LOG_FUNCTION_NOARGS();
+    return m_address;
+}
+
+bool
+BridgeP4NetDevice::SetMtu(const uint16_t mtu)
+{
+    NS_LOG_FUNCTION_NOARGS();
+    m_mtu = mtu;
+    return true;
+}
+
+uint16_t
+BridgeP4NetDevice::GetMtu() const
+{
+    NS_LOG_FUNCTION_NOARGS();
+    return m_mtu;
+}
+
+bool
+BridgeP4NetDevice::IsLinkUp() const
+{
+    NS_LOG_FUNCTION_NOARGS();
+    return true;
+}
+
+void
+BridgeP4NetDevice::AddLinkChangeCallback(Callback<void> callback)
+{
+}
+
+bool
+BridgeP4NetDevice::IsBroadcast() const
+{
+    NS_LOG_FUNCTION_NOARGS();
+    return true;
+}
+
+Address
+BridgeP4NetDevice::GetBroadcast() const
+{
+    NS_LOG_FUNCTION_NOARGS();
+    return Mac48Address::GetBroadcast();
+}
+
+bool
+BridgeP4NetDevice::IsMulticast() const
+{
+    NS_LOG_FUNCTION_NOARGS();
+    return true;
+}
+
+Address
+BridgeP4NetDevice::GetMulticast(Ipv4Address multicastGroup) const
+{
+    NS_LOG_FUNCTION(this << multicastGroup);
+    Mac48Address multicast = Mac48Address::GetMulticast(multicastGroup);
+    return multicast;
+}
+
+bool
+BridgeP4NetDevice::IsPointToPoint() const
+{
+    NS_LOG_FUNCTION_NOARGS();
+    return false;
+}
+
+bool
+BridgeP4NetDevice::IsBridge() const
+{
+    NS_LOG_FUNCTION_NOARGS();
+    return true;
+}
+
+bool
+BridgeP4NetDevice::Send(Ptr<Packet> packet, const Address& dest, uint16_t protocolNumber)
+{
+    NS_LOG_FUNCTION_NOARGS();
+    return SendFrom(packet, m_address, dest, protocolNumber);
+}
+
+bool
+BridgeP4NetDevice::SendFrom(Ptr<Packet> packet,
+                            const Address& src,
+                            const Address& dest,
+                            uint16_t protocolNumber)
+{
+    /*
+     */
+    NS_LOG_FUNCTION_NOARGS();
+    Mac48Address dst = Mac48Address::ConvertFrom(dest);
+
+    // try to use the learned state if data is unicast
+    // if (!dst.IsGroup())
+    // {
+    //     Ptr<NetDevice> outPort = GetLearnedState(dst);
+    //     if (outPort)
+    //     {
+    //         outPort->SendFrom(packet, src, dest, protocolNumber);
+    //         return true;
+    //     }
+    // }
+
+    // 广播数据包
+    // data was not unicast or no state has been learned for that mac
+    // address => flood through all ports.
+    Ptr<Packet> pktCopy;
+    for (auto iter = m_ports.begin(); iter != m_ports.end(); iter++)
+    {
+        pktCopy = packet->Copy();
+        Ptr<NetDevice> port = *iter;
+        port->SendFrom(pktCopy, src, dest, protocolNumber);
+    }
+
+    return true;
+}
+
+void
+BridgeP4NetDevice::SendPacket(Ptr<Packet> packetOut,
+                              int outPort,
+                              uint16_t protocol,
+                              const Address& destination)
+{
+    // this->SendNs3Packet(packetOut, outPort, protocol, destination);
+    return;
+}
+
+void
+BridgeP4NetDevice::SendNs3Packet(Ptr<Packet> packetOut,
+                                 int outPort,
+                                 uint16_t protocol,
+                                 const Address& destination)
+{
+    NS_LOG_DEBUG("Sending ns3 packet to port " << outPort);
+    if (packetOut)
+    {
+        EthernetHeader eeh;
+        packetOut->RemoveHeader(eeh);
+
+        if (outPort != 511)
+        {
+            NS_LOG_DEBUG("EgressPortNum: " << outPort);
+            Ptr<NetDevice> outNetDevice = GetBridgePort(outPort);
+            outNetDevice->Send(packetOut->Copy(), destination, protocol);
+        }
+    }
+    else
+        NS_LOG_DEBUG("Null Packet!");
+}
+
+Ptr<Node>
+BridgeP4NetDevice::GetNode() const
+{
+    NS_LOG_FUNCTION_NOARGS();
+    return m_node;
+}
+
+void
+BridgeP4NetDevice::SetNode(Ptr<Node> node)
+{
+    NS_LOG_FUNCTION_NOARGS();
+    m_node = node;
+}
+
+bool
+BridgeP4NetDevice::NeedsArp() const
+{
+    NS_LOG_FUNCTION_NOARGS();
+    return false;
+}
+
+void
+BridgeP4NetDevice::SetReceiveCallback(NetDevice::ReceiveCallback cb)
+{
+    NS_LOG_FUNCTION_NOARGS();
+    m_rxCallback = cb;
+}
+
+void
+BridgeP4NetDevice::SetPromiscReceiveCallback(NetDevice::PromiscReceiveCallback cb)
+{
+    NS_LOG_FUNCTION_NOARGS();
+    m_promiscRxCallback = cb;
+}
+
+bool
+BridgeP4NetDevice::SupportsSendFrom() const
+{
+    NS_LOG_FUNCTION_NOARGS();
+    return false;
+}
+
+Address
+BridgeP4NetDevice::GetMulticast(Ipv6Address addr) const
+{
+    NS_LOG_FUNCTION(this << addr);
+    return Mac48Address::GetMulticast(addr);
+}
+
+} // namespace ns3
---
+++ model/bridge-p4-net-device.h
@@ -0,0 +1,207 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2018 Stanford University
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors: Mingyu Ma <mingyu.ma@tu-dresden.de>
+ */
+
+#ifndef BRIDGE_P4_NET_DEVICE_H
+#define BRIDGE_P4_NET_DEVICE_H
+
+#include "p4-bridge-channel.h"
+#include "p4-switch-core.h"
+
+#include "ns3/mac48-address.h"
+#include "ns3/net-device.h"
+#include "ns3/nstime.h"
+
+#include <map>
+#include <stdint.h>
+#include <string>
+
+/**
+ * \file
+ * \ingroup bridge
+ * ns3::BridgeP4NetDevice declaration.
+ */
+
+namespace ns3
+{
+
+class Node;
+
+/**
+ * \defgroup bridge Bridge P4 Network Device
+ *
+ * \brief A Bridge Net Device with Programmable Data Plane
+ *
+ * P4NetDevice is a subclass of NetDevice in the ns-3 domain and serves as
+ * the network layer of a P4 target. It is compatible with other net devices
+ * in ns-3.
+ *
+ * \attention The Spanning Tree Protocol part of 802.1D is not
+ * implemented.  Therefore, you have to be careful not to create
+ * bridging loops, or else the network will collapse.
+ *
+ * \attention Bridging is designed to work only with NetDevices
+ * modelling IEEE 802-style technologies, such as CsmaNetDevice and
+ * WifiNetDevice.
+ * 
+ * \TODO Create a new channel class supporting arbitrary underlying channel.
+ */
+
+/**
+ * \ingroup bridge
+ * \brief A Bridge Net Device with Programmable Data Plane, based on that,
+ * it also can be a p4 switch in network.
+ */
+class BridgeP4NetDevice : public NetDevice
+{
+  public:
+    /**
+     * \brief Get the type ID.
+     * \return the object TypeId
+     */
+    static TypeId GetTypeId();
+
+    BridgeP4NetDevice();
+    ~BridgeP4NetDevice() override;
+
+    // Delete copy constructor and assignment operator to avoid misuse
+    BridgeP4NetDevice(const BridgeP4NetDevice&) = delete;
+    BridgeP4NetDevice& operator=(const BridgeP4NetDevice&) = delete;
+
+    /**
+     * \brief Add a 'port' to a P4 bridge device
+     * \param bridgePort the NetDevice to add
+     *
+     * This method adds a new bridge port to a BridgeP4NetDevice, so that
+     * the new bridge port NetDevice becomes part of the bridge and L2
+     * frames start being forwarded to/from this NetDevice.
+     *
+     * \attention The netdevice that is being added as bridge port must
+     * _not_ have an IP address.  In order to add IP connectivity to a
+     * bridging node you must enable IP on the BridgeNetDevice itself,
+     * never on its port netdevices.
+     */
+    void AddBridgePort(Ptr<NetDevice> bridgePort);
+
+    /**
+     * \brief Gets the number of bridged 'ports', i.e., the NetDevices currently bridged.
+     *
+     * \return the number of bridged ports.
+     */
+    uint32_t GetNBridgePorts() const;
+
+    /**
+    * \brief Gets the number ID of a 'port' connected to P4 net device.
+    * \param netdevice
+    * \return the port number of the p4 bridge device
+    */
+    uint32_t GetPortNumber(Ptr<NetDevice> port) const;
+
+    /**
+     * \brief Gets the n-th bridged port.
+     * \param n the port index
+     * \return the n-th bridged NetDevice
+     */
+    Ptr<NetDevice> GetBridgePort(uint32_t n) const;
+
+    /**
+    * \brief This method sends a packet out to the destination port.
+    * \param packetOut the packet need to be send out
+    * \param outPort the port index to send out
+    * \param protocol the packet protocol (e.g., Ethertype)
+    * \param destination the packet destination
+    */
+    void SendPacket(Ptr<Packet> packetOut, int outPort, uint16_t protocol, Address const &destination);
+
+    void SendNs3Packet(Ptr<Packet> packetOut, int outPort, uint16_t protocol, Address const &destination);
+    
+    // inherited from NetDevice base class.
+    void SetIfIndex(const uint32_t index) override;
+    uint32_t GetIfIndex() const override;
+    Ptr<Channel> GetChannel() const override;
+    void SetAddress(Address address) override;
+    Address GetAddress() const override;
+    bool SetMtu(const uint16_t mtu) override;
+    uint16_t GetMtu() const override;
+    bool IsLinkUp() const override;
+    void AddLinkChangeCallback(Callback<void> callback) override;
+    bool IsBroadcast() const override;
+    Address GetBroadcast() const override;
+    bool IsMulticast() const override;
+    Address GetMulticast(Ipv4Address multicastGroup) const override;
+    bool IsPointToPoint() const override;
+    bool IsBridge() const override;
+    bool Send(Ptr<Packet> packet, const Address& dest, uint16_t protocolNumber) override;
+    bool SendFrom(Ptr<Packet> packet,
+                  const Address& source,
+                  const Address& dest,
+                  uint16_t protocolNumber) override;
+    Ptr<Node> GetNode() const override;
+    void SetNode(Ptr<Node> node) override;
+    bool NeedsArp() const override;
+    void SetReceiveCallback(NetDevice::ReceiveCallback cb) override;
+    void SetPromiscReceiveCallback(NetDevice::PromiscReceiveCallback cb) override;
+    bool SupportsSendFrom() const override;
+    Address GetMulticast(Ipv6Address addr) const override;
+
+  protected:
+    void DoDispose() override;
+
+    /**
+     * \brief Receives a packet from one bridged port.
+     * \param device the originating port
+     * \param packet the received packet
+     * \param protocol the packet protocol (e.g., Ethertype)
+     * \param source the packet source
+     * \param destination the packet destination
+     * \param packetType the packet type (e.g., host, broadcast, etc.)
+     */
+    void ReceiveFromDevice(Ptr<NetDevice> device,
+                           Ptr<const Packet> packet,
+                           uint16_t protocol,
+                           const Address& source,
+                           const Address& destination,
+                           PacketType packetType);
+
+
+    // /**
+    //  * \brief Gets the port associated to a source address
+    //  * \param source the source address
+    //  * \returns the port the source is associated to, or NULL if no association is known.
+    //  */
+    // Ptr<NetDevice> GetLearnedState(Mac48Address source);
+
+  private:
+    NetDevice::ReceiveCallback m_rxCallback;               //!< receive callback
+    NetDevice::PromiscReceiveCallback m_promiscRxCallback; //!< promiscuous receive callback
+
+    Mac48Address m_address; //!< MAC address of the NetDevice
+    Ptr<Node> m_node;                                  //!< node owning this NetDevice
+    Ptr<P4BridgeChannel> m_channel;                      //!< virtual bridged channel
+    std::vector<Ptr<NetDevice>> m_ports;               //!< bridged ports
+    uint32_t m_ifIndex;                                //!< Interface index
+    uint16_t m_mtu;                                    //!< MTU of the bridged NetDevice
+
+    P4Switch* m_p4Switch; //!< P4 switch core
+
+};
+
+} // namespace ns3
+
+#endif /* BRIDGE_P4_NET_DEVICE_H */
