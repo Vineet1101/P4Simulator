0a7164b20999377405b308c0dae4219bed32b23a
Vineet1101 <vineetgoel692@gmail.com>
2026-02-23 03:47:19 -0800

    added dummy switch net device for traffic manager update
    Signed-off-by: Vineet1101 <vineetgoel692@gmail.com>

---
+++ model/dummy-switch-net-device.cc
@@ -0,0 +1,442 @@
+/*
+ * Copyright (c) 2025 TU Dresden
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors: P4sim Contributors
+ */
+
+#include "ns3/dummy-switch-net-device.h"
+
+#include "ns3/boolean.h"
+#include "ns3/channel.h"
+#include "ns3/ethernet-header.h"
+#include "ns3/log.h"
+#include "ns3/node.h"
+#include "ns3/packet.h"
+#include "ns3/simulator.h"
+#include "ns3/uinteger.h"
+
+namespace ns3
+{
+
+NS_LOG_COMPONENT_DEFINE("DummySwitchNetDevice");
+NS_OBJECT_ENSURE_REGISTERED(DummySwitchNetDevice);
+
+TypeId
+DummySwitchNetDevice::GetTypeId()
+{
+    static TypeId tid =
+        TypeId("ns3::DummySwitchNetDevice")
+            .SetParent<NetDevice>()
+            .SetGroupName("P4sim")
+            .AddConstructor<DummySwitchNetDevice>()
+
+            .AddAttribute("Mtu",
+                          "The MAC-level Maximum Transmission Unit",
+                          UintegerValue(1500),
+                          MakeUintegerAccessor(&DummySwitchNetDevice::SetMtu,
+                                              &DummySwitchNetDevice::GetMtu),
+                          MakeUintegerChecker<uint16_t>())
+
+            .AddTraceSource("Rx",
+                            "A packet received by the dummy switch",
+                            MakeTraceSourceAccessor(&DummySwitchNetDevice::m_rxTrace),
+                            "ns3::Packet::TracedCallback")
+
+            .AddTraceSource("Tx",
+                            "A packet transmitted by the dummy switch",
+                            MakeTraceSourceAccessor(&DummySwitchNetDevice::m_txTrace),
+                            "ns3::Packet::TracedCallback");
+
+    return tid;
+}
+
+DummySwitchNetDevice::DummySwitchNetDevice()
+    : m_node(nullptr),
+      m_ifIndex(0),
+      m_mtu(1500)
+{
+    NS_LOG_FUNCTION(this);
+    m_channel = CreateObject<P4BridgeChannel>();
+}
+
+DummySwitchNetDevice::~DummySwitchNetDevice()
+{
+    NS_LOG_FUNCTION(this);
+}
+
+void
+DummySwitchNetDevice::DoInitialize()
+{
+    NS_LOG_FUNCTION(this);
+    NS_LOG_INFO("DummySwitchNetDevice initialized with " << m_ports.size() << " ports");
+    NetDevice::DoInitialize();
+}
+
+void
+DummySwitchNetDevice::DoDispose()
+{
+    NS_LOG_FUNCTION(this);
+    for (auto& port : m_ports)
+    {
+        port = nullptr;
+    }
+    m_ports.clear();
+    m_channel = nullptr;
+    m_node = nullptr;
+    NetDevice::DoDispose();
+}
+
+// =============================================================================
+// Port management
+// =============================================================================
+
+uint32_t
+DummySwitchNetDevice::AddPort(Ptr<NetDevice> portDevice)
+{
+    NS_LOG_FUNCTION(this << portDevice);
+    NS_ASSERT(portDevice != this);
+
+    if (!Mac48Address::IsMatchingType(portDevice->GetAddress()))
+    {
+        NS_FATAL_ERROR("Device does not support EUI-48 addresses: cannot be added to switch.");
+    }
+    if (!portDevice->SupportsSendFrom())
+    {
+        NS_FATAL_ERROR("Device does not support SendFrom: cannot be added to switch.");
+    }
+
+    // Adopt the MAC of the first port added
+    if (m_address == Mac48Address())
+    {
+        m_address = Mac48Address::ConvertFrom(portDevice->GetAddress());
+    }
+
+    // Create the port wrapper
+    Ptr<DummySwitchPort> port = CreateObject<DummySwitchPort>();
+    uint32_t portId = m_ports.size();
+    port->SetPortId(portId);
+    port->SetNetDevice(portDevice);
+
+    // Register protocol handler on the node
+    NS_LOG_DEBUG("Registering protocol handler for port " << portId
+                 << " (" << portDevice->GetInstanceTypeId().GetName() << ")");
+
+    m_node->RegisterProtocolHandler(
+        MakeCallback(&DummySwitchNetDevice::ReceiveFromPort, this),
+        0,
+        portDevice,
+        true);
+
+    m_ports.push_back(port);
+    m_channel->AddChannel(portDevice->GetChannel());
+
+    NS_LOG_INFO("Added port " << portId << " to dummy switch ("
+                              << portDevice->GetInstanceTypeId().GetName() << ")");
+    return portId;
+}
+
+uint32_t
+DummySwitchNetDevice::GetNPorts() const
+{
+    return m_ports.size();
+}
+
+Ptr<DummySwitchPort>
+DummySwitchNetDevice::GetPort(uint32_t portId) const
+{
+    if (portId >= m_ports.size())
+    {
+        return nullptr;
+    }
+    return m_ports[portId];
+}
+
+int32_t
+DummySwitchNetDevice::GetPortIdForDevice(Ptr<NetDevice> device) const
+{
+    for (uint32_t i = 0; i < m_ports.size(); i++)
+    {
+        if (m_ports[i]->GetNetDevice() == device)
+        {
+            return static_cast<int32_t>(i);
+        }
+    }
+    return -1;
+}
+
+// =============================================================================
+// Traffic manager configuration
+// =============================================================================
+
+bool
+DummySwitchNetDevice::SetPortTrafficManager(uint32_t portId, Ptr<QueueDisc> qdisc)
+{
+    NS_LOG_FUNCTION(this << portId << qdisc);
+    if (portId >= m_ports.size())
+    {
+        NS_LOG_ERROR("Port " << portId << " does not exist");
+        return false;
+    }
+    m_ports[portId]->SetTrafficManager(qdisc);
+    return true;
+}
+
+// =============================================================================
+// Forwarding
+// =============================================================================
+
+void
+DummySwitchNetDevice::ReceiveFromPort(Ptr<NetDevice> device,
+                                      Ptr<const Packet> packet,
+                                      uint16_t protocol,
+                                      const Address& source,
+                                      const Address& destination,
+                                      PacketType packetType)
+{
+    NS_LOG_FUNCTION(this << device << packet << protocol << source << destination << packetType);
+
+    int32_t inPort = GetPortIdForDevice(device);
+    NS_LOG_INFO("Rx packet (uid=" << packet->GetUid()
+                << ", size=" << packet->GetSize()
+                << ") on port " << inPort);
+
+    // Fire trace
+    m_rxTrace(packet);
+
+    // Promiscuous callback
+    if (!m_promiscRxCallback.IsNull())
+    {
+        m_promiscRxCallback(this, packet, protocol, source, destination, packetType);
+    }
+
+    // Check if addressed to this switch
+    Mac48Address dst48 = Mac48Address::ConvertFrom(destination);
+    if (dst48 == m_address)
+    {
+        if (!m_rxCallback.IsNull())
+        {
+            m_rxCallback(this, packet, protocol, source);
+        }
+        return;
+    }
+
+    // === Skeleton forwarding: flood to all ports except ingress ===
+    NS_LOG_DEBUG("Flooding packet to all ports except port " << inPort);
+    for (uint32_t i = 0; i < m_ports.size(); i++)
+    {
+        if (static_cast<int32_t>(i) == inPort)
+        {
+            continue; // skip ingress port
+        }
+
+        Ptr<Packet> pktCopy = packet->Copy();
+        SendToPort(pktCopy, i, protocol, destination);
+    }
+}
+
+void
+DummySwitchNetDevice::SendToPort(Ptr<Packet> packet,
+                                 uint32_t egressPort,
+                                 uint16_t protocolNumber,
+                                 const Address& destination)
+{
+    NS_LOG_FUNCTION(this << packet << egressPort << protocolNumber << destination);
+
+    if (egressPort >= m_ports.size())
+    {
+        NS_LOG_ERROR("Egress port " << egressPort << " out of range");
+        return;
+    }
+
+    Ptr<DummySwitchPort> port = m_ports[egressPort];
+    if (!port->IsPortUp())
+    {
+        NS_LOG_DEBUG("Egress port " << egressPort << " is down, dropping");
+        return;
+    }
+
+    NS_LOG_DEBUG("Tx packet (size=" << packet->GetSize()
+                 << ") to port " << egressPort);
+
+    // Fire trace
+    m_txTrace(packet);
+
+    port->EnqueueEgress(packet, destination, protocolNumber);
+}
+
+// =============================================================================
+// NetDevice interface
+// =============================================================================
+
+void
+DummySwitchNetDevice::SetIfIndex(const uint32_t index)
+{
+    m_ifIndex = index;
+}
+
+uint32_t
+DummySwitchNetDevice::GetIfIndex() const
+{
+    return m_ifIndex;
+}
+
+Ptr<Channel>
+DummySwitchNetDevice::GetChannel() const
+{
+    return m_channel;
+}
+
+void
+DummySwitchNetDevice::SetAddress(Address address)
+{
+    m_address = Mac48Address::ConvertFrom(address);
+}
+
+Address
+DummySwitchNetDevice::GetAddress() const
+{
+    return m_address;
+}
+
+bool
+DummySwitchNetDevice::SetMtu(const uint16_t mtu)
+{
+    m_mtu = mtu;
+    return true;
+}
+
+uint16_t
+DummySwitchNetDevice::GetMtu() const
+{
+    return m_mtu;
+}
+
+bool
+DummySwitchNetDevice::IsLinkUp() const
+{
+    return true;
+}
+
+void
+DummySwitchNetDevice::AddLinkChangeCallback(Callback<void> callback)
+{
+    // Not implemented for skeleton
+}
+
+bool
+DummySwitchNetDevice::IsBroadcast() const
+{
+    return true;
+}
+
+Address
+DummySwitchNetDevice::GetBroadcast() const
+{
+    return Mac48Address::GetBroadcast();
+}
+
+bool
+DummySwitchNetDevice::IsMulticast() const
+{
+    return true;
+}
+
+Address
+DummySwitchNetDevice::GetMulticast(Ipv4Address multicastGroup) const
+{
+    return Mac48Address::GetMulticast(multicastGroup);
+}
+
+Address
+DummySwitchNetDevice::GetMulticast(Ipv6Address addr) const
+{
+    return Mac48Address::GetMulticast(addr);
+}
+
+bool
+DummySwitchNetDevice::IsPointToPoint() const
+{
+    return false;
+}
+
+bool
+DummySwitchNetDevice::IsBridge() const
+{
+    return true;
+}
+
+bool
+DummySwitchNetDevice::Send(Ptr<Packet> packet, const Address& dest, uint16_t protocolNumber)
+{
+    NS_LOG_FUNCTION(this << packet << dest << protocolNumber);
+    return SendFrom(packet, m_address, dest, protocolNumber);
+}
+
+bool
+DummySwitchNetDevice::SendFrom(Ptr<Packet> packet,
+                               const Address& source,
+                               const Address& dest,
+                               uint16_t protocolNumber)
+{
+    NS_LOG_FUNCTION(this << packet << source << dest << protocolNumber);
+
+    // Flood to all ports
+    for (uint32_t i = 0; i < m_ports.size(); i++)
+    {
+        Ptr<Packet> pktCopy = packet->Copy();
+        Ptr<NetDevice> portDev = m_ports[i]->GetNetDevice();
+        portDev->SendFrom(pktCopy, source, dest, protocolNumber);
+    }
+    return true;
+}
+
+Ptr<Node>
+DummySwitchNetDevice::GetNode() const
+{
+    return m_node;
+}
+
+void
+DummySwitchNetDevice::SetNode(Ptr<Node> node)
+{
+    m_node = node;
+}
+
+bool
+DummySwitchNetDevice::NeedsArp() const
+{
+    return true;
+}
+
+void
+DummySwitchNetDevice::SetReceiveCallback(NetDevice::ReceiveCallback cb)
+{
+    m_rxCallback = cb;
+}
+
+void
+DummySwitchNetDevice::SetPromiscReceiveCallback(NetDevice::PromiscReceiveCallback cb)
+{
+    m_promiscRxCallback = cb;
+}
+
+bool
+DummySwitchNetDevice::SupportsSendFrom() const
+{
+    return true;
+}
+
+} // namespace ns3
---
+++ model/dummy-switch-net-device.h
@@ -0,0 +1,203 @@
+/*
+ * Copyright (c) 2025 TU Dresden
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors: P4sim Contributors
+ */
+
+#ifndef DUMMY_SWITCH_NET_DEVICE_H
+#define DUMMY_SWITCH_NET_DEVICE_H
+
+#include "ns3/dummy-switch-port.h"
+#include "ns3/mac48-address.h"
+#include "ns3/net-device.h"
+#include "ns3/p4-bridge-channel.h"
+#include "ns3/traced-callback.h"
+
+#include <string>
+#include <vector>
+
+namespace ns3
+{
+
+class Node;
+
+/**
+ * \ingroup p4sim
+ * \brief A skeleton dummy switch NetDevice for the P4sim Plan A architecture.
+ *
+ * DummySwitchNetDevice implements a minimal multi-port switch following the
+ * bridge pattern used by ns3::BridgeNetDevice and ns3::P4SwitchNetDevice.
+ * Each port is wrapped in a DummySwitchPort object that supports an optional
+ * egress Traffic Manager (QueueDisc).
+ *
+ * This skeleton does not contain any real packet processing logic — it
+ * performs simple learning-bridge-style forwarding (flood if destination
+ * unknown). It serves as the architectural starting point for the full
+ * P4 switch implementation.
+ */
+class DummySwitchNetDevice : public NetDevice
+{
+  public:
+    /**
+     * \brief Get the type ID.
+     * \return the object TypeId
+     */
+    static TypeId GetTypeId();
+
+    DummySwitchNetDevice();
+    ~DummySwitchNetDevice() override;
+
+    // Delete copy constructor and assignment operator
+    DummySwitchNetDevice(const DummySwitchNetDevice&) = delete;
+    DummySwitchNetDevice& operator=(const DummySwitchNetDevice&) = delete;
+
+    // === Port management ===
+
+    /**
+     * \brief Add a port to the switch.
+     *
+     * Wraps the given NetDevice in a DummySwitchPort and registers
+     * a protocol handler to receive frames from it.
+     *
+     * \param portDevice the NetDevice to add as a switch port
+     * \return the port index assigned to this device
+     */
+    uint32_t AddPort(Ptr<NetDevice> portDevice);
+
+    /**
+     * \brief Get the number of ports on this switch.
+     * \return the number of ports
+     */
+    uint32_t GetNPorts() const;
+
+    /**
+     * \brief Get a port by its index.
+     * \param portId the port index
+     * \return the DummySwitchPort, or nullptr if index out of range
+     */
+    Ptr<DummySwitchPort> GetPort(uint32_t portId) const;
+
+    /**
+     * \brief Get the port index for a given NetDevice.
+     * \param device the NetDevice to look up
+     * \return the port index, or -1 if not found
+     */
+    int32_t GetPortIdForDevice(Ptr<NetDevice> device) const;
+
+    // === Traffic Manager configuration ===
+
+    /**
+     * \brief Attach a traffic manager (QueueDisc) to a specific egress port.
+     * \param portId the port index
+     * \param qdisc the QueueDisc to attach
+     * \return true if successfully attached
+     */
+    bool SetPortTrafficManager(uint32_t portId, Ptr<QueueDisc> qdisc);
+
+    // === Forwarding ===
+
+    /**
+     * \brief Send a packet out a specific port.
+     *
+     * Delegates to the port's EnqueueEgress method, which may
+     * pass through a traffic manager if one is attached.
+     *
+     * \param packet the packet to send
+     * \param egressPort the egress port index
+     * \param protocolNumber the protocol number
+     * \param destination the destination address
+     */
+    void SendToPort(Ptr<Packet> packet,
+                    uint32_t egressPort,
+                    uint16_t protocolNumber,
+                    const Address& destination);
+
+    // === NetDevice interface overrides ===
+    void SetIfIndex(const uint32_t index) override;
+    uint32_t GetIfIndex() const override;
+    Ptr<Channel> GetChannel() const override;
+    void SetAddress(Address address) override;
+    Address GetAddress() const override;
+    bool SetMtu(const uint16_t mtu) override;
+    uint16_t GetMtu() const override;
+    bool IsLinkUp() const override;
+    void AddLinkChangeCallback(Callback<void> callback) override;
+    bool IsBroadcast() const override;
+    Address GetBroadcast() const override;
+    bool IsMulticast() const override;
+    Address GetMulticast(Ipv4Address multicastGroup) const override;
+    Address GetMulticast(Ipv6Address addr) const override;
+    bool IsPointToPoint() const override;
+    bool IsBridge() const override;
+    bool Send(Ptr<Packet> packet, const Address& dest, uint16_t protocolNumber) override;
+    bool SendFrom(Ptr<Packet> packet,
+                  const Address& source,
+                  const Address& dest,
+                  uint16_t protocolNumber) override;
+    Ptr<Node> GetNode() const override;
+    void SetNode(Ptr<Node> node) override;
+    bool NeedsArp() const override;
+    void SetReceiveCallback(NetDevice::ReceiveCallback cb) override;
+    void SetPromiscReceiveCallback(NetDevice::PromiscReceiveCallback cb) override;
+    bool SupportsSendFrom() const override;
+
+  protected:
+    void DoInitialize() override;
+    void DoDispose() override;
+
+    /**
+     * \brief Handle a packet received from one of the switch ports.
+     *
+     * In the skeleton, this performs simple flood-based forwarding:
+     * the packet is sent out all ports except the ingress port.
+     *
+     * \param device the originating port NetDevice
+     * \param packet the received packet
+     * \param protocol the packet protocol
+     * \param source the source address
+     * \param destination the destination address
+     * \param packetType the packet type
+     */
+    void ReceiveFromPort(Ptr<NetDevice> device,
+                         Ptr<const Packet> packet,
+                         uint16_t protocol,
+                         const Address& source,
+                         const Address& destination,
+                         PacketType packetType);
+
+  private:
+    // === Port storage ===
+    std::vector<Ptr<DummySwitchPort>> m_ports; //!< List of switch ports
+
+    // === Network device information ===
+    Mac48Address m_address;              //!< MAC address of switch
+    Ptr<Node> m_node;                    //!< Node that owns this NetDevice
+    Ptr<P4BridgeChannel> m_channel;      //!< Virtual bridge channel
+    uint32_t m_ifIndex;                  //!< Interface index
+    uint16_t m_mtu;                      //!< MTU
+
+    // === Callbacks ===
+    NetDevice::ReceiveCallback m_rxCallback;               //!< Receive callback
+    NetDevice::PromiscReceiveCallback m_promiscRxCallback;  //!< Promiscuous receive callback
+
+    // === Trace sources ===
+    TracedCallback<Ptr<const Packet>> m_rxTrace;   //!< Rx trace
+    TracedCallback<Ptr<const Packet>> m_txTrace;   //!< Tx trace
+};
+
+} // namespace ns3
+
+#endif /* DUMMY_SWITCH_NET_DEVICE_H */
