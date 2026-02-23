d013550a0dd369c72e7f62e66f10d8e9c803f1dc
Vineet1101 <vineetgoel692@gmail.com>
2026-02-23 03:46:12 -0800

    added dummy port for traffic manager update
    Signed-off-by: Vineet1101 <vineetgoel692@gmail.com>

---
+++ model/dummy-switch-port.cc
@@ -0,0 +1,175 @@
+/*
+ * Copyright (c) 2025 TU Dresden
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors: P4sim Contributors
+ */
+
+#include "ns3/dummy-switch-port.h"
+
+#include "ns3/log.h"
+#include "ns3/queue-disc.h"
+#include "ns3/uinteger.h"
+
+namespace ns3
+{
+
+NS_LOG_COMPONENT_DEFINE("DummySwitchPort");
+NS_OBJECT_ENSURE_REGISTERED(DummySwitchPort);
+
+TypeId
+DummySwitchPort::GetTypeId()
+{
+    static TypeId tid = TypeId("ns3::DummySwitchPort")
+                            .SetParent<Object>()
+                            .SetGroupName("P4sim")
+                            .AddConstructor<DummySwitchPort>();
+    return tid;
+}
+
+DummySwitchPort::DummySwitchPort()
+    : m_netDevice(nullptr),
+      m_portId(0),
+      m_egressTm(nullptr),
+      m_isUp(true)
+{
+    NS_LOG_FUNCTION(this);
+}
+
+DummySwitchPort::~DummySwitchPort()
+{
+    NS_LOG_FUNCTION(this);
+}
+
+void
+DummySwitchPort::DoDispose()
+{
+    NS_LOG_FUNCTION(this);
+    m_netDevice = nullptr;
+    m_egressTm = nullptr;
+    Object::DoDispose();
+}
+
+void
+DummySwitchPort::SetNetDevice(Ptr<NetDevice> device)
+{
+    NS_LOG_FUNCTION(this << device);
+    m_netDevice = device;
+}
+
+Ptr<NetDevice>
+DummySwitchPort::GetNetDevice() const
+{
+    return m_netDevice;
+}
+
+void
+DummySwitchPort::SetPortId(uint32_t portId)
+{
+    NS_LOG_FUNCTION(this << portId);
+    m_portId = portId;
+}
+
+uint32_t
+DummySwitchPort::GetPortId() const
+{
+    return m_portId;
+}
+
+void
+DummySwitchPort::SetTrafficManager(Ptr<QueueDisc> qdisc)
+{
+    NS_LOG_FUNCTION(this << qdisc);
+    m_egressTm = qdisc;
+    NS_LOG_INFO("Port " << m_portId << ": Traffic manager "
+                        << (qdisc ? "attached" : "detached"));
+}
+
+Ptr<QueueDisc>
+DummySwitchPort::GetTrafficManager() const
+{
+    return m_egressTm;
+}
+
+bool
+DummySwitchPort::HasTrafficManager() const
+{
+    return (m_egressTm != nullptr);
+}
+
+void
+DummySwitchPort::SetPortUp(bool isUp)
+{
+    NS_LOG_FUNCTION(this << isUp);
+    m_isUp = isUp;
+}
+
+bool
+DummySwitchPort::IsPortUp() const
+{
+    return m_isUp;
+}
+
+bool
+DummySwitchPort::EnqueueEgress(Ptr<Packet> packet,
+                               const Address& dest,
+                               uint16_t protocolNumber)
+{
+    NS_LOG_FUNCTION(this << packet << dest << protocolNumber);
+
+    if (!m_isUp)
+    {
+        NS_LOG_DEBUG("Port " << m_portId << " is down, dropping packet");
+        return false;
+    }
+
+    if (!m_netDevice)
+    {
+        NS_LOG_ERROR("Port " << m_portId << " has no underlying NetDevice");
+        return false;
+    }
+
+    if (m_egressTm)
+    {
+        // Enqueue through the traffic manager
+        NS_LOG_DEBUG("Port " << m_portId << ": enqueueing packet (size "
+                             << packet->GetSize() << ") into traffic manager");
+        // NOTE: Full QueueDisc integration requires QueueDiscItem wrapping.
+        // For the skeleton, we bypass the TM and send directly, but log
+        // that the TM is present.
+        NS_LOG_INFO("Port " << m_portId << ": TM present ("
+                            << m_egressTm->GetInstanceTypeId().GetName()
+                            << "), forwarding packet directly for now");
+    }
+
+    // Send directly on the underlying NetDevice
+    NS_LOG_DEBUG("Port " << m_portId << ": sending packet (size "
+                         << packet->GetSize() << ") on " 
+                         << m_netDevice->GetInstanceTypeId().GetName());
+
+    return m_netDevice->Send(packet, dest, protocolNumber);
+}
+
+void
+DummySwitchPort::TransmitFromQueue()
+{
+    NS_LOG_FUNCTION(this);
+    // Placeholder: will be used when full QueueDisc integration is implemented.
+    // The QueueDisc dequeue callback would invoke this method to actually
+    // transmit the packet on the underlying NetDevice.
+    NS_LOG_DEBUG("Port " << m_portId << ": TransmitFromQueue called (placeholder)");
+}
+
+} // namespace ns3
---
+++ model/dummy-switch-port.h
@@ -0,0 +1,141 @@
+/*
+ * Copyright (c) 2025 TU Dresden
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors: P4sim Contributors
+ */
+
+#ifndef DUMMY_SWITCH_PORT_H
+#define DUMMY_SWITCH_PORT_H
+
+#include "ns3/net-device.h"
+#include "ns3/object.h"
+#include "ns3/packet.h"
+#include "ns3/queue-disc.h"
+
+namespace ns3
+{
+
+/**
+ * \ingroup p4sim
+ * \brief Represents a single port on a DummySwitchNetDevice.
+ *
+ * DummySwitchPort wraps an underlying ns-3 NetDevice (e.g. CsmaNetDevice,
+ * PointToPointNetDevice) and adds an optional egress Traffic Manager (QueueDisc)
+ * slot. When a QueueDisc is attached, egress packets are enqueued into it;
+ * otherwise they are forwarded directly to the underlying NetDevice.
+ */
+class DummySwitchPort : public Object
+{
+  public:
+    /**
+     * \brief Get the type ID.
+     * \return the object TypeId
+     */
+    static TypeId GetTypeId();
+
+    DummySwitchPort();
+    ~DummySwitchPort() override;
+
+    /**
+     * \brief Set the underlying NetDevice for this port.
+     * \param device the NetDevice to attach
+     */
+    void SetNetDevice(Ptr<NetDevice> device);
+
+    /**
+     * \brief Get the underlying NetDevice.
+     * \return the NetDevice attached to this port
+     */
+    Ptr<NetDevice> GetNetDevice() const;
+
+    /**
+     * \brief Set the port index.
+     * \param portId the port index on the switch
+     */
+    void SetPortId(uint32_t portId);
+
+    /**
+     * \brief Get the port index.
+     * \return the port index on the switch
+     */
+    uint32_t GetPortId() const;
+
+    /**
+     * \brief Attach a traffic manager (QueueDisc) to this egress port.
+     * \param qdisc the QueueDisc to use for egress traffic management
+     */
+    void SetTrafficManager(Ptr<QueueDisc> qdisc);
+
+    /**
+     * \brief Get the traffic manager attached to this port.
+     * \return the QueueDisc, or nullptr if none is attached
+     */
+    Ptr<QueueDisc> GetTrafficManager() const;
+
+    /**
+     * \brief Check whether this port has a traffic manager attached.
+     * \return true if a QueueDisc is attached
+     */
+    bool HasTrafficManager() const;
+
+    /**
+     * \brief Set the administrative up/down state of this port.
+     * \param isUp true to set port up, false to set port down
+     */
+    void SetPortUp(bool isUp);
+
+    /**
+     * \brief Check whether this port is administratively up.
+     * \return true if port is up
+     */
+    bool IsPortUp() const;
+
+    /**
+     * \brief Enqueue a packet for egress on this port.
+     *
+     * If a traffic manager is attached, the packet is enqueued into
+     * the QueueDisc. Otherwise, it is sent directly on the underlying
+     * NetDevice.
+     *
+     * \param packet the packet to send
+     * \param dest the destination address
+     * \param protocolNumber the protocol number
+     * \return true if successfully enqueued or sent
+     */
+    bool EnqueueEgress(Ptr<Packet> packet,
+                       const Address& dest,
+                       uint16_t protocolNumber);
+
+  protected:
+    void DoDispose() override;
+
+  private:
+    /**
+     * \brief Transmit a packet from the queue onto the underlying NetDevice.
+     *
+     * This is the callback invoked when the QueueDisc dequeues a packet.
+     */
+    void TransmitFromQueue();
+
+    Ptr<NetDevice> m_netDevice; //!< The underlying ns-3 NetDevice
+    uint32_t m_portId;          //!< Port index on the switch
+    Ptr<QueueDisc> m_egressTm;  //!< Optional egress traffic manager (QueueDisc)
+    bool m_isUp;                //!< Administrative up/down state
+};
+
+} // namespace ns3
+
+#endif /* DUMMY_SWITCH_PORT_H */
